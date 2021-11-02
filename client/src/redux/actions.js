e56ed7f98ba725b300e3202958dd1619c60f857f
Juan Moldes <juanignaciomoldes1@gmail.com>
2021-11-01 21:06:15 -0300

    Lunes 1 redux

---
+++ client/src/redux/actions.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+export const GET_ALL_COUNTRIES = 'GET_ALL_COUNTRIES';
+export const CREATE_ACTIVITY = 'CREATE_ACTIVITY';
+export const GET_COUNTRY_BY_ID = 'GET_COUNTRY_BY_ID';
+export const SET_NAME = 'SET_NAME';
+export const SET_PAGE = 'SET_PAGE';
+export const FILTER_CONTINENT = 'FILTER_CONTINENT';
+export const FILTER_CONTINENT = 'FILTER_CONTINENT';
+export const ORDER_POPULATION = 'ORDER_POPULATION';
+export const ORDER = 'ORDER';
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,7 +1,11 @@
 
 
 
-const initialState = {}
+const initialState = {
+  countries: [],
+  countriesModifed: [],
+  
+}
 
 function reducer (state = initialState, { type, payload }){
   switch (type) {
