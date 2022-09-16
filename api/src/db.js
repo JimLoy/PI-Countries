6dce8fdf1bd0536da1a25ea0e259b138cc0e299a
Juan Moldes <juanignaciomoldes1@gmail.com>
2022-09-16 15:35:42 -0300

    package.json fixed

--- api/src/db.js
+++ api/src/db.js
@@ -3,32 +3,35 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const environment = process.env.NODE_ENV;
 
 let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false
-          },
-          keepAlive: true
-        },
-        ssl: true
-      })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, { logging: false, native: false });
-
+	environment === 'production'
+		? new Sequelize({
+				database: DB_NAME,
+				dialect: 'postgres',
+				host: DB_HOST,
+				port: 5432,
+				username: DB_USER,
+				password: DB_PASSWORD,
+				pool: {
+					max: 3,
+					min: 1,
+					idle: 10000,
+				},
+				dialectOptions: {
+					ssl: {
+						require: true,
+						rejectUnauthorized: false,
+					},
+					keepAlive: true,
+				},
+				ssl: true,
+		  })
+		: new Sequelize(
+				`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+				{ logging: false, native: false }
+		  );
 
 const basename = path.basename(__filename);
 
@@ -36,16 +39,22 @@ const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
-  });
+	.filter(
+		file =>
+			file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+	)
+	.forEach(file => {
+		modelDefiners.push(require(path.join(__dirname, '/models', file)));
+	});
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map(entry => [
+	entry[0][0].toUpperCase() + entry[0].slice(1),
+	entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
@@ -53,11 +62,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { Country, Activity, CountryActivity } = sequelize.models;
 
 // Aca vendrian las relaciones
-Country.belongsToMany(Activity,{through: CountryActivity });
-Activity.belongsToMany(Country,{through: CountryActivity });
-
+Country.belongsToMany(Activity, { through: CountryActivity });
+Activity.belongsToMany(Country, { through: CountryActivity });
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+	...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+	conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
