5ea2c5e5d03525a79857bfbde9ba9ac80e3a1385
Juan Ignacio Moldes <juanignaciomoldes1@gmail.com>
2022-09-16 17:16:12 -0300

    update cors

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-countries-jimloy.vercel.app'); 
+  res.header('Access-Control-Allow-Origin', 'https://countries-jimloy.vercel.app'); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
